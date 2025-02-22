82443a4829ca1016bb5b43c7b78ad64b496730d3
Dharani <dharanipaidipalli@gmail.com>
2025-02-22 19:45:07 +0530

    adding all files

---
+++ src/pages/Home.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+     <img src="/ban.jpg" class="img-fluid w-100 pt-1" alt="..."/ >
+     <div class="row row-cols-1 row-cols-md-2 g-4 m-2">
+  <div class="col">
+    <div class="card">
+      <img src="/ban.jpg" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Veg Recipes</h5>
+        <p class="card-text">"Wholesome veggies, perfect flavors, and a touch of love—cooking made delicious! 🥦🍲✨"</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/ban1.jpg" class="card-img-top" alt="..."/ >
+      <div class="card-body">
+        <h5 class="card-title">Nonveg Recepies</h5>
+        <p class="card-text">"Rich flavors, tender bites, and irresistible aroma—perfect non-veg delight! 🍗🔥🍛"</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/ban2.jpg" class="card-img-top" alt="..."/ >
+      <div class="card-body">
+        <h5 class="card-title">Desserts making</h5>
+        <p class="card-text">"Sweet creations, heavenly flavors, and pure delight in every bite! 🍰🍫✨"</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/ban3.jpg" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">Salad Making</h5>
+        <p class="card-text">"Fresh, crisp, and bursting with vibrant flavors—salads made delicious! 🥗✨"</p>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Home
